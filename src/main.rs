9ba54333a500672c69add9013faff43030b7b47c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-03-02 11:03:40 +0100

    Add graceful shutdown handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tracing;
 use tracing_subscriber;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
@@ -10,5 +11,27 @@ async fn main() {
     let app = observatory::routes::router();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).with_graceful_shutdown(shutdown_handler()).await.unwrap();
+}
+
+async fn shutdown_handler() {
+    let ctrl_c = async {
+        signal::ctrl_c().await.expect("Failed to create Ctrl+C handler")
+    };
+
+    #[cfg(unix)]
+        let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("Failed to create signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => { tracing::info!("Shutting down") },
+        _ = terminate => { tracing::info!("Shutting down") },
+    }
 }
